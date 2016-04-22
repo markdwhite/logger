e841cb2957e694df4f2b861b1cc13620f0c103d8
Mark White <mark@somsip.com>
2016-04-22 12:31:53 +0700

    Changed undefined output

--- src/Formatter/CallerInlineFormatter.php
+++ src/Formatter/CallerInlineFormatter.php
@@ -5,7 +5,21 @@ namespace Somsip\Logger\Formatter;
 use Monolog\Formatter\LineFormatter;
 
 /**
- * Formats incoming records into a one-line string prefixed by the calling Class::function()
+ * Formats incoming records into a one-line string prefixed by the calling Class::method()
+ *
+ * Examples:
+ *
+ * Class and method
+ * [2000-01-01 00:00:00] app.DEBUG: Class::method() Test message
+ *
+ * Function call
+ * [2000-01-01 00:00:00] app.DEBUG: function() Test message
+ *
+ * Class but method not identified
+ * [2000-01-01 00:00:00] app.DEBUG: Class::{undefined}() Test message
+ *
+ * No class or function identified
+ * [2000-01-01 00:00:00] app.DEBUG: Test message
  *
  * @author Mark White <mark@somsip.com>
  */
@@ -22,10 +36,14 @@ class CallerInlineFormatter extends LineFormatter
         if ($class = $this->extractClass($record['extra'])) {
             $class .= "::";
         }
-        if (!$function = $this->extractFunction($record['extra'])) {
-            $function = '{undefined}';
+        if ($function = $this->extractFunction($record['extra'])) {
+            $function .= "() ";
+        } else {
+            if ($class) {
+                $function = '{undefined}() ';
+            } 
         }
-        $record['message'] = sprintf('%s%s() %s', $class, $function, $record['message']);
+        $record['message'] = sprintf('%s%s%s', $class, $function, $record['message']);
 
         return parent::format($record);
     }
--- tests/Formatter/CallerInlineFormatterTest.php
+++ tests/Formatter/CallerInlineFormatterTest.php
@@ -14,69 +14,79 @@ class CallerInlineFormatterTest extends PHPUnit_Framework_TestCase
     public function testFormatDefault()
     {
         $formatter = new CallerInlineFormatter();
-        $mockRecord = [
-            'message' => 'Test message',
-            'channel' => 'phpunit',
-            'datetime' => '2000-01-01 00:00:00',
-            'level_name' => 'TEST',
-            'context' => [],
-            'extra' => [
+        $extra = [
                 'class' => '\\Tests\Unit\\Class',
                 'function' => 'test',
                 'line' => '123',
                 'file' => '/var/www/test.php'
-            ]
         ];
+        $mockRecord = $this->mockRecord($extra);
+
 
         $result = $formatter->format($mockRecord);
         // Trim trailing newline
         $result = trim($result);
 
         $this->assertEquals('[2000-01-01 00:00:00] phpunit.TEST: Class::test() Test message', $result);
-
     }
 
     public function testFormatNoClass()
     {
         $formatter = new CallerInlineFormatter();
-        $mockRecord = [
-            'message' => 'Test message',
-            'channel' => 'phpunit',
-            'datetime' => '2000-01-01 00:00:00',
-            'level_name' => 'TEST',
-            'context' => [],
-            'extra' => [
+        $extra = [
                 'function' => 'test',
-            ]
         ];
+        $mockRecord = $this->mockRecord($extra);
 
         $result = $formatter->format($mockRecord);
         // Trim trailing newline
         $result = trim($result);
 
         $this->assertEquals('[2000-01-01 00:00:00] phpunit.TEST: test() Test message', $result);
-
     }
 
     public function testFormatNoFunction()
     {
         $formatter = new CallerInlineFormatter();
-        $mockRecord = [
-            'message' => 'Test message',
-            'channel' => 'phpunit',
-            'datetime' => '2000-01-01 00:00:00',
-            'level_name' => 'TEST',
-            'context' => [],
-            'extra' => [
-                'class' => '\\Tests\Unit\\Class',
-            ]
+        $extra = [
+            'class' => '\\Tests\Unit\\Class',
         ];
+        $mockRecord = $this->mockRecord($extra);
 
         $result = $formatter->format($mockRecord);
         // Trim trailing newline
         $result = trim($result);
 
         $this->assertEquals('[2000-01-01 00:00:00] phpunit.TEST: Class::{undefined}() Test message', $result);
+    }
 
+    public function testFormatNoExtras()
+    {
+        $formatter = new CallerInlineFormatter();
+        $mockRecord = $this->mockRecord();
+
+        $result = $formatter->format($mockRecord);
+        // Trim trailing newline
+        $result = trim($result);
+
+        $this->assertEquals('[2000-01-01 00:00:00] phpunit.TEST: Test message', $result);
+    }
+
+    /**
+     * Provides a mock record for use in other tests
+     *
+     * @param array $extra
+     * @return array
+     */
+    private function mockRecord(array $extra = [])
+    {
+        return [
+            'message' => 'Test message',
+            'channel' => 'phpunit',
+            'datetime' => '2000-01-01 00:00:00',
+            'level_name' => 'TEST',
+            'context' => [],
+            'extra' => $extra
+        ];
     }
 }
